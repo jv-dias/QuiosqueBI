7a0f9f469f1b001d0954cb759a153e7d16c5ada6
João Victor Dias <jvictor.dev@outlook.com>
2025-07-12 01:53:19 -0300

    Backup antes da reestruturação

--- backend/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238eb0400c671964e345a18b474f92340913f0f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc74df89acd114485cee81b960108b2ab9ec6939")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
